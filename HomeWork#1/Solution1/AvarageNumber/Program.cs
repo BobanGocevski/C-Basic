0af7e68b2173615b623203138c72c687a4ad62e2
BobanGocevski <117583530+BobanGocevski@users.noreply.github.com>
2023-03-09 23:10:06 +0100

    HomeWork1

---
+++ HomeWork#1/Solution1/AvarageNumber/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ Create new console application “RealCalculator” that takes two numbers from the input and asks what 
+operation would the user want to be done ( +, - , * , / ). Then it returns the result.
+
+Test Data:
+Enter the First number: 10
+Enter the Second number: 15
+Enter the Operation: +
+Expected Output:
+The result is: 25
+ */
+
+Console.WriteLine("Please enter your  operation:");
+string operation = Console.ReadLine();
+
+if (operation != "+" && operation != "-" && operation != "*" && operation != "/")
+{
+    Console.WriteLine("Invalid operation selected!!!");
+    System.Environment.Exit(1);
+
+
+}
+
+Console.WriteLine("Enter the First number:");
+string numberOne = Console.ReadLine();
+int number1 = int.Parse(numberOne);
+
+Console.WriteLine("Enter the second number:");
+string numberTwo = Console.ReadLine();
+int number2 = int.Parse(numberTwo);
+
+if (operation == "+")
+{
+    Console.WriteLine($"The result is: {number1 + number2}");
+}
+else if (operation == "-")
+{
+    Console.WriteLine($"The result is: {number1 - number2}");
+}
+else if (operation == "*")
+{
+    Console.WriteLine($"The result is: {number1 * number2}");
+}
+else if (operation == "/")
+{
+    Console.WriteLine($"The result is: {number1 / number2}");
+}
+
+Console.Read();
---
+++ HomeWork#1/Solution1/SwapNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Input the first number:");
+string numb1 = Console.ReadLine();
+int numby1 = int.Parse(numb1);
+Console.WriteLine($"Before the swap the first number is equal to {numby1}");
+
+Console.WriteLine("Input the second number:");
+string numb2 = Console.ReadLine();
+int numby2 = int.Parse(numb2);
+Console.WriteLine($"Before the swap the first number is equal to {numby2}");
+
+numby1 = numby1 + numby2;
+numby2 = numby1 - numby2;
+numby1 = numby1 - numby2;
+
+Console.WriteLine($"After the swap the first number is equal to {numby1}");
+Console.WriteLine($"After the swap the second number is equal to {numby2}");
+
+
+Console.Read();
