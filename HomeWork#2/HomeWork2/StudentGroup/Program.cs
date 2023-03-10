a902fc9aff2c588937e04b554a17a11d491e40c0
BobanGocevski <117583530+BobanGocevski@users.noreply.github.com>
2023-03-11 00:23:08 +0100

    HomeWork2

---
+++ HomeWork#2/HomeWork2/StudentGroup/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] studentsG1 = new string[] { "Rajko", "Spiro", "Kirko", "AnA", "Vanco" };
+string[] studentsG2 = new string[] { "Petar", "Jovana", "Kire", "Angela", "Goce" };
+
+Console.Write("Enter student group (1 or 2): ");
+int groupNumber = int.Parse(Console.ReadLine());
+
+
+if (groupNumber == 1)
+{
+    Console.WriteLine("The students in G1 are:");
+    foreach (string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}
+else if (groupNumber == 2)
+{
+    Console.WriteLine("The students in G2 are:");
+    foreach (string student in studentsG2)
+    {
+        Console.WriteLine(student);
+    }
+}
+else
+{
+    Console.WriteLine("Invalid group number!");
+}
+
+
---
+++ HomeWork#2/HomeWork2/SumOfEven/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] numbers = new int [] { 1, 5, 8, 9, 10, 25, 40 };
+int sum = 0;
+
+foreach (int number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        sum += number;
+    }
+}
+
+Console.WriteLine($"The result is: {sum}");
+
