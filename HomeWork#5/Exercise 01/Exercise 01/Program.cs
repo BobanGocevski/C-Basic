ca25ddbdd9c6d2b0a85bd1f3835234c02a41285d
BobanGocevski <117583530+BobanGocevski@users.noreply.github.com>
2023-03-28 22:04:31 +0200

    HomeWork#5

---
+++ HomeWork#5/Exercise 01/Exercise 01/Program.cs
@@ -0,0 +1,23 @@
+﻿Rectangle rect = new Rectangle();
+rect.Name = "Rectangle";
+rect.Color = "Blue";
+rect.Position = new int[] { 0, 0 };
+rect.SideA = 5;
+rect.SideB = 3;
+
+Circle circle = new Circle();
+circle.Name = "Circle";
+circle.Color = "Green";
+circle.Position = new int[] { 10, 10 };
+circle.Radius = 4;
+
+Shape.move(rect);
+Shape.move(circle);
+
+Console.WriteLine($"Rectangle - Name: {rect.Name}, Color: {rect.Color}, Position: ({rect.Position[0]}, {rect.Position[1]}), SideA: {rect.SideA}, SideB: {rect.SideB}");
+rect.getArea(); 
+rect.getPerimeter(); 
+
+Console.WriteLine($"Circle - Name: {circle.Name}, Color: {circle.Color}, Position: ({circle.Position[0]}, {circle.Position[1]}), Radius: {circle.Radius}");
+circle.getArea(); 
+circle.getPerimeter(); 
---
+++ HomeWork#5/Exercise 01/Exercise 01/Shape.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+class Shape
+{
+    private string _name;
+    private string _color;
+    public int[] Position { get; set; }
+
+    public string Name
+    {
+        get { return _name; }
+        set
+        {
+            Console.WriteLine($"Setting name to {value}.");
+            _name = value;
+        }
+    }
+
+    public string Color
+    {
+        get { return _color; }
+        set
+        {
+            if (value == "")
+            {
+                Console.WriteLine("Color cannot be empty.");
+            }
+            else
+            {
+                Console.WriteLine($"Setting color to {value}.");
+                _color = value;
+            }
+        }
+    }
+
+    public void getArea()
+    {
+        Console.WriteLine("There is no special implementation for area.");
+    }
+
+    public void getPerimeter()
+    {
+        Console.WriteLine("There is no special implementation for perimeter.");
+    }
+
+    public static void move(Shape shape)
+    {
+        shape.Position[0] += 5;
+        shape.Position[1] += 5;
+    }
+}
+
+class Rectangle : Shape
+{
+    public int SideA { get; set; }
+    public int SideB { get; set; }
+
+    public  void getArea()
+    {
+        Console.WriteLine($"Area of rectangle {Name} is {SideA * SideB}.");
+    }
+
+    public  void getPerimeter()
+    {
+        Console.WriteLine($"Perimeter of rectangle {Name} is {(SideA + SideB) * 2}.");
+    }
+}
+
+class Circle : Shape
+{
+    public int Radius { get; set; }
+
+    public  void getArea()
+    {
+        Console.WriteLine($"Area of circle {Name} is {Math.PI * Radius * Radius}.");
+    }
+
+    public void getPerimeter()
+    {
+        Console.WriteLine($"Circumference of circle {Name} is {2 * Math.PI * Radius}.");
+    }
+}
